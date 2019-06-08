0242c7d4ae6cd13c70e7760a8585242a80f1d892
ninxfeet <matho.camara@gmail.com>
2019-06-08 08:24:00 +0300

    Lab5:

--- CPlusLab5/CPlusLab5.cpp
+++ CPlusLab5/CPlusLab5.cpp
@@ -1,21 +1,149 @@
-// CPlusLab5.cpp : This file contains the 'main' function. Program execution begins and ends there.
+﻿// CPlusLab5.cpp : This file contains the 'main' function. Program execution begins and ends there.
 //
 
 #include "pch.h"
 #include <iostream>
+#include <string>
+#include <fstream>
+#include <ctime>
+
+using namespace std;
+
+int goodNumber();
 
 int main()
 {
-    std::cout << "Hello World!\n"; 
+	srand(time(0));
+
+	cout << "Enter a 'k' value:\n";
+	int k = goodNumber();
+	cout << "Enter a 'l' value:\n";
+	int l = goodNumber();
+	cout << "Enter a 'n' Matrix size:\n";
+	int n = goodNumber();
+
+	//---------------------------------------------------
+	//запись
+
+	ofstream output;
+	//first file
+	output.open("C:\\Users\\matho\\source\\repos\\CPlusLab1\\CPlusLab1\\first.txt");
+	if (output.is_open())
+	{
+		//checking and adding matrices to the file
+		if (k != 1 && l > k)
+		{
+			for (int i = 0; i < l - k; i++)
+			{
+				for (int j = 0; j < n; j++)
+				{
+					for (int h = 0; h < n; h++)
+					{
+						output << (j == h ? 1 : 0) << " ";
+					}
+					output << "\n";
+				}
+				output << "\n----------\n\n";
+			}
+		}
+
+		for (int i = 0; i < k; i++)
+		{
+			for (int j = 0; j < n; j++)
+			{
+				for (int h = 0; h < n; h++)
+				{
+					output << ((rand() % 9) + 1) << " ";
+				}
+				output << "\n";
+			}
+			output << "\n----------\n\n";
+		}
+
+
+	}
+	output.close();
+
+	//second file
+	output.open("C:\\Users\\matho\\source\\repos\\CPlusLab1\\CPlusLab1\\second.txt");
+	if (output.is_open())
+	{
+		//checking and adding matrix to the file
+		if (k > l)
+		{
+			for (int i = 0; i < k - l; i++)
+			{
+				for (int j = 0; j < n; j++)
+				{
+					for (int h = 0; h < n; h++)
+					{
+						output << (j == h ? 1 : 0) << " ";
+					}
+					output << "\n";
+				}
+				output << "\n----------\n\n";
+			}
+		}
+		for (int i = 0; i < l; i++)
+		{
+			int r = (rand() % 9) + 1;
+			for (int j = 0; j < r; j++)
+			{
+				for (int h = 0; h < r; h++)
+				{
+					output << ((rand() % 9) + 1) << " ";
+				}
+				output << "\n";
+			}
+			output << "\n----------\n\n";
+		}
+	}
+	output.close();
+
+	//---------------------------------------------------
+	//READING
+
+	//first file
+	cout << "\n\nReading first file...\n----------\n\n";
+	string line;
+	ifstream input1("C:\\Users\\matho\\source\\repos\\CPlusLab1\\CPlusLab1\\first.txt");
+	if (input1.is_open())
+	{
+		while (getline(input1, line))
+		{
+			cout << line << "\n";
+		}
+	}
+	input1.close();
+
+	//second file
+	cout << "\n\nReading second file...\n----------\n\n";
+	ifstream input2("C:\\Users\\matho\\source\\repos\\CPlusLab1\\CPlusLab1\\second.txt");
+	if (input2.is_open())
+	{
+		while (getline(input2, line))
+		{
+			cout << line << "\n";
+		}
+	}
+	input2.close();
+
+	//-------------------------------------------------
+
+	system("pause");
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
+//checking the entered number
+int goodNumber()
+{
+	string s = "c";
+	int n = 0;
 
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
+	while (n <= 0 || n > 9)
+	{
+		cout << "Enter a number between 1 and 9:\n";
+		getline(cin, s);
+		n = atoi(s.c_str());
+	}
+	return n;
+}
